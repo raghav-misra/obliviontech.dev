3719508f24cc544767a4b4d470c46790225b6a74
Raghav Misra <raghav.m2014@gmail.com>
2020-05-11 19:05:30 -0700

    Add basic typedjs todo homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,28 @@
 import { h, Fragment } from "../parts/jsx";
+import Typed from "typed.js";
 
-export default <Fragment>
-    <h2>Homepage!</h2>
-</Fragment>
+const Page = <Fragment>
+    <hr />
+    <h3 id="serviceCycler" class="mono"></h3>
+    <hr />
+    <style>{`
+        .typed-cursor { display: none; }
+    `}</style>
+</Fragment>;
+
+export default Page;
+
+const showSkills = new Typed(Page.querySelector("#serviceCycler"), {
+    strings: [
+        "full-stack web apps",
+        "unity c# games",
+        "windows desktop apps",
+        "obliviontech",
+    ],
+
+    typeSpeed: 50,
+    backSpeed: 37.5,
+    
+    startDelay: 500,
+    backDelay: 500
+});
